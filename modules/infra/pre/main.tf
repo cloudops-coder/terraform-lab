e28109257796e1eaaf1449f6e476205f78f1bcdc
cloudops-coder <98970605+cloudops-coder@users.noreply.github.com>
2025-05-26 09:40:56 +0530

    Delete modules directory

--- modules/infra/pre/main.tf
+++
@@ -1,4 +0,0 @@
-module "rg" {
-source = "../../rg"
-rg_names = {}
-}
--- modules/rg/rg.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-    for_each = var.rg_names
-    name = each.value.name
-    location = each.value.location
-    tags = {
-        environment = each.value.environment
-    }
-}
