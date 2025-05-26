ef36a261062952c093d7e5045725d7262e78da26
bsnlmahesh <mahesh.23joy@gmail.com>
2025-05-26 09:38:53 +0530

    modules block lab added

---
+++ modules/rg/rg.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "rg" {
+    for_each = var.rg_names
+    name = each.value.name
+    location = each.value.location
+    tags = {
+        environment = each.value.environment
+    }
+}
